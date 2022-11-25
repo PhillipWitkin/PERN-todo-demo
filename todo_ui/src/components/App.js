4abcf896b8530eb4e79c034ad48969916c98916a
Phillip Witkin <pwitkin273@gmail.com>
2022-11-25 01:23:37 -0500

    altered folder structure

--- todo_ui/src/components/App.js
+++ todo_ui/src/components/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from '../styles/logo.svg'
+import '../styles/App.css';
 
 function App() {
   return (
     <div className="App">
+      {/* <h1>Hello</h1> */}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+  
     </div>
   );
 }
