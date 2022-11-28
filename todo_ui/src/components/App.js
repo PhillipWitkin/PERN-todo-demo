83dd3cd32c12c5f9bc542ae219f1ad96cece17b2
Phillip Witkin <pwitkin273@gmail.com>
2022-11-28 00:44:33 -0500

    commiting remaining files

--- todo_ui/src/components/App.js
+++ todo_ui/src/components/App.js
@@ -1,28 +1,121 @@
-import logo from '../styles/logo.svg'
+// import logo from '../styles/logo.svg'
+import React, { useState, useRef, useEffect } from "react";
 import '../styles/App.css';
 import Todo from "./Todo";
+import Form from "./Form";
+import FilterButton from "./FilterButton";
+import { nanoid } from "nanoid";
+
+
+function usePrevious(value) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
+
+const FILTER_MAP = {
+  All: () => true,
+  Active: task => !task.completed,
+  Completed: task => task.completed
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
 
 function App(props) {
 
-  const taskList = props.tasks.map((task) => (
+  const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState('All');
+
+  function addTask(name) {
+    console.log("Adding task name: ", name);
+    const newTask = { id: "todo-" + nanoid(), name: name, completed: false };
+    setTasks([...tasks, newTask]);
+  }
+
+  function toggleTaskCompleted(id) {
+    console.log("Toggling todo id:", id);
+    const updatedTasks = tasks.map(task => {
+      // if this task has the same ID as the edited task
+      if (id === task.id) {
+        // use object spread to make a new obkect
+        // whose `completed` prop has been inverted
+        return {...task, completed: !task.completed}
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+
+
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter(task => id !== task.id);
+    setTasks(remainingTasks);
+  }
+
+
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map(task => {
+    // if this task has the same ID as the edited task
+      if (id === task.id) {
+        //
+        return {...task, name: newName}
+      }
+      return task;
+    });
+    setTasks(editedTaskList);
+  }
+
+
+  const taskList = tasks
+  .filter(FILTER_MAP[filter])
+  .map((task) => (
     <Todo 
       id={task.id} 
       name={task.name} 
       completed={task.completed}
+      key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
+      editTask={editTask}
     />
   ));
+
+  const filterList = FILTER_NAMES.map(name => (
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
+    />
+  ));
+
+  const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
+
+  const listHeadingRef = useRef(null);
+  const prevTaskLength = usePrevious(tasks.length);
+
+  useEffect(() => {
+    if (tasks.length - prevTaskLength === -1) {
+      listHeadingRef.current.focus();
+    }
+  }, [tasks.length, prevTaskLength]);
+
 
   return (
     <div className="todoapp stack-large">
     <Form addTask={addTask} />
-      {/* <div className="filters btn-group stack-exception">
+      <div className="filters btn-group stack-exception">
         {filterList}
-      </div> */}
-      {/* <h2 id="list-heading" tabIndex="-1" ref={listHeadingRef}>
+      </div>
+      <h2 id="list-heading" tabIndex="-1" ref={listHeadingRef}>
         {headingText}
-      </h2> */}
+      </h2>
       <ul
-          role="list"
+          // role="list"
           className="todo-list stack-large stack-exception"
           aria-labelledby="list-heading"
         >
--- todo_ui/src/components/Form.js
+++ todo_ui/src/components/Form.js
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Form(props) {
+  
+  const [name, setName] = useState('');
+
+  function handleSubmit(e) {
+    console.log("Input submitted...", name);
+    e.preventDefault();
+    if (!name.trim()) {
+      return;
+    }
+    props.addTask(name);
+    setName("");
+  }
+
+  function handleChange(e) {
+    console.log("Input new task name changed...", e.target.value);
+    setName(e.target.value);
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2 className="label-wrapper">
         <label htmlFor="new-todo-input" className="label__lg">
           What needs to be done?
@@ -14,6 +32,8 @@ function Form(props) {
         className="input input__lg"
         name="text"
         autoComplete="off"
+        value={name}
+        onChange={handleChange}
       />
       <button type="submit" className="btn btn__primary btn__lg">
         Add
