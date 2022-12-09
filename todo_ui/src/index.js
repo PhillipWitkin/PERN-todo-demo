0bfa308da3e7c0cecd3e026ce6aed8bd63936dae
Phillip Witkin <pwitkin273@gmail.com>
2022-12-09 07:02:32 -0500

    commented out UI mock data

--- todo_ui/src/index.js
+++ todo_ui/src/index.js
@@ -4,11 +4,11 @@ import './styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const DATA = [
-  { id: "todo-0", name: "Eat", completed: true },
-  { id: "todo-1", name: "Sleep", completed: false },
-  { id: "todo-2", name: "Repeat", completed: false }
-];
+// const DATA = [
+//   { id: "todo-0", name: "Eat", completed: true },
+//   { id: "todo-1", name: "Sleep", completed: false },
+//   { id: "todo-2", name: "Repeat", completed: false }
+// ];
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
